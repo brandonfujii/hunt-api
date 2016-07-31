f8cc7cb5a14c38e3db77dbfd34168e5c93e28f87
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-07-30 22:27:32 -0400

    merged biggie's changes. we all go through changes

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ var _ = require('underscore');
 
 // GET /users
 module.exports.getUsers = function(cb, limit) {
-  User.find(cb).limit(limit);
+  User.find(cb).limit(limit).sort('points');
 }
 
 // GET /users/:id
